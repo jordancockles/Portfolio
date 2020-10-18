9cf705cb7a4b12427ef985599119493f05bef31d
jordancockles <jordan.cockles@gmail.com>
2020-10-18 20:12:45 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const startedPersonal = new Date(Date.parse('01 Aug 2010'));
+const startedProfessional = new Date(Date.parse('01 Aug 2015'));
+const totalTimeSpan = 'total-time';
+const totalProfessionalTimeSpan = 'total-professional';
+
+document.querySelector('.portfolio-button').addEventListener("click", event => {
+    event.preventDefault();
+    console.log("Stub Function Activated");
+})
+
+window.onload = function WindowLoad(event) {
+    setNumOfYears(startedPersonal, totalTimeSpan);
+    setNumOfYears(startedProfessional, totalProfessionalTimeSpan);
+}
+
+function setNumOfYears(startDate, span){
+    var numOfYears = new Date().getFullYear() - startDate.getFullYear();
+    document.getElementById(span).innerHTML = numOfYears;
+}
