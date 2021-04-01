bfdc6f5eac61f35d96ef96cd9d106abe0f02df84
jordancockles <jordan.cockles@gmail.com>
2021-04-01 20:22:26 +0100

    Adding Swup framework for page transitions

--- js/index.js
+++ js/index.js
@@ -1,21 +1,13 @@
+const swup = new Swup()
 const startedPersonal = new Date(Date.parse('01 Aug 2010'));
 const startedProfessional = new Date(Date.parse('01 Aug 2015'));
 const totalTimeSpan = 'total-time';
 const totalProfessionalTimeSpan = 'total-professional';
 
-document.querySelector('.portfolio-button').addEventListener("click", event => {
-    event.preventDefault();
-    console.log("Stub Function Activated");
-})
-
 window.onload = function WindowLoad(event) {
     setNumOfYears(startedPersonal, totalTimeSpan);
     setNumOfYears(startedProfessional, totalProfessionalTimeSpan);
     document.querySelector('body').style.opacity = 1
-}
-
-window.transitionToPage = function() {
-    document.querySelector('body').style.opacity = 0
 }
 
 function setNumOfYears(startDate, span){
