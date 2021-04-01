9fbe9d590ee0ffd020a1a6778955c9d840488c62
jordancockles <jordan.cockles@gmail.com>
2021-04-01 20:52:54 +0100

    Removing automated script for years reference

--- js/index.js
+++ js/index.js
@@ -1,16 +1,5 @@
 const swup = new Swup()
-const startedPersonal = new Date(Date.parse('01 Aug 2010'));
-const startedProfessional = new Date(Date.parse('01 Aug 2015'));
-const totalTimeSpan = 'total-time';
-const totalProfessionalTimeSpan = 'total-professional';
 
 window.onload = function WindowLoad(event) {
-    setNumOfYears(startedPersonal, totalTimeSpan);
-    setNumOfYears(startedProfessional, totalProfessionalTimeSpan);
     document.querySelector('body').style.opacity = 1
-}
-
-function setNumOfYears(startDate, span){
-    var numOfYears = new Date().getFullYear() - startDate.getFullYear();
-    document.getElementById(span).innerHTML = numOfYears;
 }
